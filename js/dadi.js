622f7d92967c897fd0deb51e331c6a2008da13e9
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-13 17:10:36 +0200

    push

---
+++ js/dadi.js
@@ -0,0 +1,33 @@
+const userNumber = [1,2,3,4,5,6]
+const pcNumber = [1,2,3,4,5,6]
+let msg = ""
+let match = false
+
+for (let x = 0; x < userNumber.length; x++) {
+    const element = Math.floor(Math.random() * userNumber[x]);
+    console.log(element)
+
+    for (let y = 0; y < pcNumber.length; y++) {
+        const pcNum = Math.floor(Math.random() * pcNumber[y]);
+        
+      
+    }
+
+    if (element > luis){
+        match = true
+       
+    }
+
+    else{
+    }
+    
+}
+
+if (match){
+    msg = "hai vinto"
+    console.log=msg
+}
+else{
+    msg ="hai perso"
+    console.log=msg
+}
--- js/mail.js
+++ js/mail.js
@@ -1,19 +1,31 @@
 // chiedo l'email al mio utente
-const emailUser = prompt("Inserire la propria E-mail")
+const emailUser = prompt("Inserire la propria E-mail");
 
 // creo un contenitore per le email di sistema
-const recordMail = ["matteotosini300@icloud.com","daniele@gmail.it","alessio@gmail.it"]
+const recordMail = ["matteotosini300@icloud.com","daniele@gmail.it","alessio@gmail.it"];
+let messageUser = "";
 
-
+let match = false
 
 for (let x = 0; x < recordMail.length; x++) {
     const mail = recordMail[x];
-    console.log(mail)
-    // se 
+    console.log(mail);
+    
+    
     if(emailUser === mail){
-        alert("Complimenti accesso effettuato")
+      match = true
+      console.log("sono dentro l'if")
     }
-    if(emailUser != mail){
-        alert("l'email non è presente nel sistema")
+    else{
+       console.log("sono dentro l'else")
     }
+
 }
+if (match){
+    messageUser = "Complimenti hai effettuato l'accesso" 
+}
+else{
+    messageUser= "l'email non è presente nel sistema"
+}
+
+alert(messageUser)
